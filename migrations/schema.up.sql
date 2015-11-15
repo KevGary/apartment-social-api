c5a559d1f92766ccdeda2d82587ddb4fd8cfc9b8
KevGary <kevgary.dev@gmail.com>
2015-11-15 05:28:47 -0700

    substantial progress on routes within api

--- migrations/schema.up.sql
+++ migrations/schema.up.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS apartments;
+DROP TABLE IF EXISTS leases;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS status;
 
 CREATE TABLE users (
   id serial primary key,
@@ -10,9 +13,29 @@ CREATE TABLE users (
 CREATE TABLE apartments (
   id serial primary key,
   name varchar(60),
-  apartment_number,
+  apartment_number integer,
   street_address varchar(100),
   city varchar(60),
   state varchar(60),
-  zip_code numeric
+  zip_code integer
+);
+
+-- CREATE TABLE leases (
+--   id serial primary key,
+--   user_id int references users(id) ON DELETE cascade,
+--   apartment_id int references apartments(id) ON DELETE cascade
+-- );
+
+CREATE TABLE messages (
+  id serial primary key,
+  content varchar(120),
+  timestamp varchar(20)
+);
+
+
+CREATE TABLE status (
+  id serial primary key,
+  option_1 varchar(80),
+  option_2 varchar(80),
+  option_3 varchar(80)
 );
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var conString = process.env.DATABASE_URL;
 
 //Users
 //get all
-router.get('/api/v1/users', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
@@ -23,30 +23,14 @@ router.get('/api/v1/users', function(req, res, next) {
     });
   });
 });
-//get one
-router.get('/api/v1/users/:id', function(req, res, next) {
+//insert user
+router.post('/users', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
     }
     console.log("connected to database");
-    client.query('SELECT * FROM users WHERE id = $1', [req.body], function(err, result) {
-      done();
-      if (err) {
-        return console.error('error running query', err);
-      }
-       res.send(result);
-    });
-  });
-});
-//insert one
-router.post('/api/v1/users', function(req, res, next) {
-  pg.connect(conString, function(err, client, done) {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
-    console.log("connected to database");
-    client.query('INSERT INTO users(first_name, last_name) VALUES($1, $2) returning id', ['donald','trumpy'], function(err, result) {
+    client.query('INSERT INTO users(first_name, last_name) VALUES($1, $2) returning id', [req.body.data.attributes.first_name,req.body.data.attributes.last_name], function(err, result) {
       done();
       if(err) {
         return console.error('error running query', err);
@@ -56,9 +40,27 @@ router.post('/api/v1/users', function(req, res, next) {
     });
   });
 })
+//get one
+router.get('/users/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM users WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      console.log(req.params.id)
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+});
 
 //Apartments
-router.get('/api/v1/apartments', function(req, res, next) {
+//get all
+router.get('/apartments', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
       return console.error('error fetching client from pool', err);
@@ -66,6 +68,112 @@ router.get('/api/v1/apartments', function(req, res, next) {
     console.log("connected to database");
     client.query('SELECT * FROM apartments', function(err, result) {
       done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+});
+//insert apartment
+router.post('/apartments', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('INSERT INTO apartments(name, apartment_number, street_address, city, state, zip_code) VALUES($1, $2, $3, $4, $5, $6) returning id', 
+      [req.body.data.attributes.name, req.body.data.attributes.apartment_number, req.body.data.attributes.street_address, req.body.data.attributes.city, req.body.data.attributes.state,
+      req.body.data.attributes.zip_code], function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+      //output: 1 
+    });
+  });
+})
+//get one
+router.get('/apartments/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM apartments WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      console.log(req.params.id)
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+});
+
+//Leases
+//get all
+router.get('/leases', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM leases', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+});
+
+//Messages
+//get all
+router.get('/messages', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM messages', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+       res.send(result);
+    });
+  });
+});
+//insert message
+router.post('/messages', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('INSERT INTO messages(content, timestamp) VALUES($1, $2) returning id', [req.body.data.attributes.content, req.body.data.attributes.timestamp], function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+      //output: 1 
+    });
+  });
+})
+//get one
+router.get('/messages/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM messages WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      console.log(req.params.id)
       if (err) {
         return console.error('error running query', err);
       }
